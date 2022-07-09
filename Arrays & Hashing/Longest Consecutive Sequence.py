8958ef7d4c299dc1d5db77b17a8fb99134b3fbd0
David P <davidpavlicek123@gmail.com>
2022-07-08 15:19:23 -1000

    Finished

--- Arrays & Hashing/Longest Consecutive Sequence.py
+++ Arrays & Hashing/Longest Consecutive Sequence.py
@@ -3,6 +3,24 @@ class Solution:
         if len(nums) == 0:
             return 0 
         
-        number_set = 
+        num_set = list(set(nums))
+        
+        num_in_row = 1
+        current_nums_row = 1
+        num_set.sort()
+
+        tracking_num = num_set[0]
+        for numbers in range(1, len(num_set)):
+            if (tracking_num + 1) != num_set[numbers]:
+                if current_nums_row > num_in_row: 
+                    num_in_row = current_nums_row
+                current_nums_row = 1
+            else: 
+                current_nums_row += 1      
+            tracking_num = num_set[numbers]
+        if current_nums_row > num_in_row: 
+            num_in_row = current_nums_row
+
+        return num_in_row
 
         
