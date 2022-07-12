f44a174286bdd66f0f2a9a770e9a70a42eddfacf
DavidPav123 <davidpavlicek123@gmail.com>
2022-07-11 20:48:57 -1000

    Finished Arrays

--- Arrays & Hashing/Valid Sudoku.py
+++ Arrays & Hashing/Valid Sudoku.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         #check Rows
-        for board_row in range(len(board)):
+        for board_row in range(9):
             seen_nums = []
             for sub_num in board[board_row]:
                 if sub_num not in seen_nums and sub_num != ".":
@@ -23,6 +23,18 @@ class Solution:
                     return False
                 
         #Check Squares
-        
+        for row_mod in range(0, 9, 3):
+            for col_mod in range(0, 9, 3):
+                seen_nums = []
+                for small_column in range(0+col_mod,3+col_mod):
+                    for small_rows in range(0+row_mod, 3+row_mod):
+                        if (board[small_rows][small_column] not in seen_nums) and (board[small_rows][small_column] != "."):
+                            seen_nums.append(board[small_rows][small_column])
+                        elif board[small_rows][small_column] == ".":
+                            continue
+                        else: 
+                            return False
 
         return True
+
+
