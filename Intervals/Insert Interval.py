799cf02f926fba9d1a362e527fb5d43663215fde
DavidPav123 <davidpavlicek123@gmail.com>
2022-07-13 20:58:35 -1000

    Finished for the day

--- Intervals/Insert Interval.py
+++ Intervals/Insert Interval.py
@@ -1,3 +1,7 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
+        int_start = 0
+        sub_int = 0
+
+        for interval in range(len(interval)):
+            if 
